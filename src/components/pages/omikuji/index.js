35931c8d9db5af5a7d0df35cccabfb377b480c97
YuyaMoritaa <m.yuya0728@gmail.com>
2020-12-26 03:10:17 +0900

    おみくじの試作ができました #8

--- src/components/pages/omikuji/index.js
+++ src/components/pages/omikuji/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import funcOmikuji from '../../funcs/funcOmikuji';
+//import omikujiPlay from './omikujiPlay';
 
 class Omikuji extends React.Component {
+  
+  omikuji = () => {
+    const ra = Math.round(Math.floor( Math.random() * 100))
+    console.log(ra);
+    switch(ra%5){
+      case 0:
+        console.log("大吉")
+        break;
+      case 1:
+        console.log("中吉")
+        break;
+      case 2:
+        console.log("小吉")
+        break;
+      case 3:
+        console.log("吉")
+        break;
+      case 4:
+        console.log("凶")
+        break;
+      default:
+        console.log("err")
+        break;
+    }
+    
+  }
+
+  //classes = useStyles();
+
   render(){
     return(
       <div>
         <h1>{this.props.name}</h1>
-        <h2>おみくじを引いてください。</h2>
-        <funcOmikuji />
+        <h2>画像をクリックして、おみくじを引こう!</h2>
+        <button onClick={this.omikuji}>Click Here</button>
       </div>
     )
   }
---
+++ src/components/pages/omikuji/omikujiPlay.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class omikujiPlay extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                <button>Click Here</button>
+            </div>
+        )
+    }
+
+}
+
+export default omikujiPlay;
