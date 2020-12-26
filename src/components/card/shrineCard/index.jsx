040b04b0c86dbfd97943e1d1d3bb6c636713493b
Soshi-1114 <4649chopper@gmail.com>
2020-12-26 19:28:43 +0900

    神社候補を選択できるようにした #12

---
+++ src/components/card/shrineCard/index.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import Isejin from '../../../assets/img/shrine/isejin.webp';
+import Izumo from '../../../assets/img/shrine/izumo.jpg';
+import Dazaifu from '../../../assets/img/shrine/dazaifu.jpg';
+import Miyajidake from '../../../assets/img/shrine/miyajidake.jpg';
+import Miyajima from '../../../assets/img/shrine/miyajima.jpg';
+import Inari from '../../../assets/img/shrine/inari.jpg';
+
+import ImgCard from '../imgCard/index';
+import Grid from "@material-ui/core/Grid";
+import { useHistory } from 'react-router-dom';
+
+
+export default function ShrineCard() {
+  const history = useHistory();
+
+  return (
+    <Grid container>
+      <Grid item xs={3}>
+        <div>
+          <ImgCard 
+          imgName={Isejin}
+          title={"Isejin Image"}
+          text={"伊勢神宮"}
+          link={()=>history.push('/omikuji')}
+          />
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+        <ImgCard 
+        imgName={Izumo}
+        title={"Izumo Image"}
+        text={"出雲大社"}
+        link={()=>history.push('/omikuji')}
+        />
+      </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+        <ImgCard 
+        imgName={Dazaifu}
+        title={"Dazaifu Image"}
+        text={"太宰府天満宮"}
+        link={()=>history.push('/omikuji')}
+        />
+      </div>
+      </Grid>
+      <Grid item xs={3}>
+      <div>
+          <ImgCard 
+          imgName={Miyajidake}
+          title={"Miyajidake Image"}
+          text={"宮地嶽神社"}
+          link={()=>history.push('/omikuji')}
+          />
+        </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+        <ImgCard 
+        imgName={Miyajima}
+        title={"Miyajima Image"}
+        text={"厳島神社"}
+        link={()=>history.push('/omikuji')}
+        />
+      </div>
+      </Grid>
+      <Grid item xs={3}>
+        <div>
+        <ImgCard 
+        imgName={Inari}
+        title={"Inari Image"}
+        text={"伏見稲荷神社"}
+        link={()=>history.push('/omikuji')}
+        />
+      </div>
+      </Grid>
+    </Grid>
+  );
+}
--- src/components/pages/shrine/index.jsx
+++ src/components/pages/shrine/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Map from './map';
+import ShrineCard from '../../card/shrineCard/index';
 
 class Shrine extends React.Component {
+  
   render(){
     return(
       <div>
         <h1>{this.props.name}</h1>
         <h2>神社を選んでください。</h2> 
-        <Map />
+        <ShrineCard />
       </div>
     )
   }
 }
-
 
 export default Shrine;
