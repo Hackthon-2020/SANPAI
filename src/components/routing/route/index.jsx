e215bdf94b4222a2389d538383e32fe26f79ef35
Soshi-1114 <4649chopper@gmail.com>
2020-12-26 20:34:46 +0900

    コンフリクト解消vol.2 #12

--- src/components/routing/route/index.jsx
+++ src/components/routing/route/index.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../navigation/index';
-import Home from '../../pages/home/index';
 import Omikuji from '../../pages/omikuji/index';
-import Osaisen from '../../pages/osaisen/index';
+import Home from '../../pages/home/index';
 // import Shrine from '../../pages/shrine/index';
-// import Footer from '../footer/index';
+import Osaisen from '../../pages/osaisen/index';
+
 
 class Routing extends Component {
   render() {
@@ -16,9 +16,8 @@ class Routing extends Component {
             <Navigation />
             <Route exact path='/' render={ () => <Home name={'ホームページ'}/> }/>
             <Route path='/omikuji' render={ () => <Omikuji name={'おみくじ'}/> }/>
-            <Route path='/osaisen' render={ () => <Osaisen name={'参拝'}/> }/>
+            <Route path='/osaisen' render={ () => <Osaisen name={'お賽銭'}/> }/>
             {/* <Route path='/shrine' render={ () => <Shrine name={'神社を見つける'}/> }/> */}
-            {/* <Footer /> */}
           </div>
         </Router>
       </React.Fragment>
