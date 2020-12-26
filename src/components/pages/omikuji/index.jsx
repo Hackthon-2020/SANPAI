f4dc842673ca9e4eb200b572b936b19f41099c81
YuyaMoritaa <m.yuya0728@gmail.com>
2020-12-26 23:15:48 +0900

    メッセージの表示まで完了です。 #8

--- src/components/pages/omikuji/index.jsx
+++ src/components/pages/omikuji/index.jsx
@@ -2,37 +2,16 @@ import React from 'react'
 //import omikujiPlay from './omikujiPlay';
 //import ImgCard from '../../card/imgCard/index'
 // import omikujiImg from '../../../assets/img/omikuji/omikuji-box.png'
-import CostomizedDialogs from './omikujiRes'
+import OmikujiPlay from './omikujiPlay'
 
 class Omikuji extends React.Component {
-  
-  omikuji = () => {
-    const ra = Math.round(Math.floor( Math.random() * 100))
-    console.log(ra);
-    switch(ra%5){
-      case 0:
-        console.log("大吉")
-        //const linkPath = './daiKichi'
-        break;
-      case 1:
-        console.log("中吉")
-        break;
-      case 2:
-        console.log("小吉")
-        break;
-      case 3:
-        console.log("吉")
-        break;
-      case 4:
-        console.log("凶")
-        break;
-      default:
-        console.log("err")
-        break;
-    }
-    
-  }
-
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     resOmikuji: null,
+  //     msgOmikuji: "",
+  //   }
+  // }
   //classes = useStyles();
 
   render(){
@@ -43,7 +22,8 @@ class Omikuji extends React.Component {
         <h1>{this.props.name}</h1>
         <h2>画像をクリックして、おみくじを引こう!</h2>
         {/* <img src={omikujiImg} onClick={this.omikuji} alt="" /> */}
-        <div><CostomizedDialogs /></div>
+        <div><OmikujiPlay /></div>
+        {/* omikujiState={this.state} */}
       </div>
       
     )
